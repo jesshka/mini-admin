0a4f976bd20373113dcac47f12f83c5dc4a126b9
Jessica <jessibrandy7@gmail.com>
2021-03-26 22:50:24 -0500

    Markup home

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,8 +1,23 @@
+// Libraries
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+// Styles
+import './Home.css';
+
 const Home = () => {
   return (
-    <div>
-      <span>HOME!</span>
-    </div>
+    <Container maxWidth="md" className="container">
+      <Typography variant="h2" className="title">Mini Admin</Typography>
+      <div className="buttons-container">
+        <Button variant="contained" color="primary" href="/usuarios">
+          Usuarios
+        </Button>
+        <Button variant="contained" color="primary" href="/ubicaciones">
+          Ubicaciones
+        </Button>
+      </div>
+    </Container>
   );
 }
 
